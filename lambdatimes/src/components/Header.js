d4cab6c5cf6786c0f3f1a8886279206dbf4aab5c
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-03 15:46:43 -0400

    created styledHeader style component based on .header class index.css

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
-
+const styledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background-color: #fff;
+  border-bottom: 1px solid lightgrey;
+  width: 100%;
+  height: 120px;
+  margin-top: 44px;
+  padding-bottom: 15px;
+`;
 const Header = () => {
   return (
     <div className="header">
