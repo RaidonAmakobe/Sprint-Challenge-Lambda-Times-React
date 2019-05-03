37adbfaff260c4398c8b7f31525736bb017ba07d
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-03 15:51:33 -0400

    styledDate and styledTemp styled components based on .header css styles

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -13,9 +13,25 @@ const styledHeader = styled.div`
   width: 100%;
   height: 120px;
   margin-top: 44px;
-  padding-bottom: 15px;
-`;
+  padding-bottom: 15px;`;
 
+const styledDate = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  margin-left: 25px;
+  flex: 1;`;
+
+const styledTemp = styled.span`
+  align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+  text-align: right;
+  margin-right: 25px
+  flex: 1;`;
+  
 const Header = () => {
   return (
     <styledHeader>
