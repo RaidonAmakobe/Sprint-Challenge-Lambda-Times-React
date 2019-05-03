b9ac27c2bea9d58bd0eafcf5129406006ab75bbb
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-03 15:57:33 -0400

    created topbar styled components

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -2,6 +2,50 @@ import React from 'react';
 
 // Refactor this component to use styled components and not classNames. 
 // You can find the corresponding CSS in the CSS/index.css file
+const superTopBar = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  position: fixed;
+  height: 44px;
+  background-color: #333;`;
+
+const babyShark = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: none;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  letter-spacing: 1px;
+  padding: 0 10px;`;
+
+const otherShark = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex: 3;
+  font-size: 9px;`
+
+const leftShark = styled.div`
+  display: flex;
+  justify-contentL none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;`;
+
+const rightShark = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  font-weight: bold;`;
 
 const TopBar = () => {
   return (
