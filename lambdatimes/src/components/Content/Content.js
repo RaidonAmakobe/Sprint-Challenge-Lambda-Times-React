b663ad4671934bd52b5ebd04494180fd23ec3d9d
Raidon Amakobe <Raidonmgmt@gmail.com>
2019-05-03 15:01:02 -0400

    created filterCards if statement first clause

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -44,6 +44,9 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
+    if(this.state.selected = "all") {
+      return this.state.cards;
+    }
     return this.state.cards;
   };
 
